37c477e272e6b7dfed010194cbf24cff67195af8
Vincent Muller <v.muller55000@gmail.com>
2023-08-11 11:46:08 +0200

    modif

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,15 @@
 import React from 'react'
-
+/**
+ * 
+ * @param {Style} modalStyle take style in prop to change style of modal
+ * @param {Style} headerStyle take style in prop to change style of header
+ * @param {Style} contentStyle take style in prop to change style of content
+ * @param {Style} footerStyle take style in prop to change style of footer
+ * @param {any} Header take any data to set header content
+ * @param {any} content take any data to set body content
+ * @param {any} footer take any data to set footer content
+ * @returns JSX elements to display a modal 
+ */
 const Modal = ({  modalStyle, headerStyle, contentStyle, footerStyle, header, content, footer }) => {
     return (
           <div className="modal" style={modalStyle}>
